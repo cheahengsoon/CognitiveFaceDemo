529f7c89db4ddc07877f57868e614fc7e1f6a92b
tonkun-no <tonkun_no@ae.auone-net.jp>
2017-06-02 03:43:33 +0900

    SUBSCRIPTION_KEYを変更

--- CognitiveFaceDemo/CognitiveFaceDemo/Const.cs
+++ CognitiveFaceDemo/CognitiveFaceDemo/Const.cs
@@ -9,8 +9,8 @@ namespace CognitiveFaceDemo
     public class Const
     {
         // cognitive service
-        public const string SUBSCRIPTION_KEY = "0ae3d43feb3b4894ac46afae47155fd3";
-        public const string GROUP_ID = "srsys-e-messe-demo";
+        public const string SUBSCRIPTION_KEY = "xxxxxxxxxx";
+        public const string GROUP_ID = "demo";
         public const string PARAM_PERSON = "person";
     }
 }
